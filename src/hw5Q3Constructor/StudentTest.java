cced4abaf83e462346288c41e9d93b5720873272
Tanvir7246 <tanvirmsaimoon@gmail.com>
2023-06-08 02:31:25 -0400

    update

--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -1,13 +1,11 @@
 package hw5Q3Constructor;
 
-public class StudentTest { public static void main(String[] args) {	
-	Student student = new Student();
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student();
 
+		Student Tanvir = new Student("Tanvir", 1045, 'M', false, 4.22f);
 
-Student Tanvir = new Student ("Tanvir", 1045, 'M', false, 4.22f);
-
-	
-}
-
+	}
 
 }
